02a7dde8eaf08cc8deb862b48eebb1d46939d119
Dipak Shelke <dipakshelke918@gmail.com>
2025-02-11 12:08:05 +0530

    first commit

---
+++ question.js
@@ -0,0 +1,123 @@
+let questions= [
+          {
+                    numb :1,
+                    question :"Which of the following is not a programming language?",
+                    answer : "C. HTML",
+                    Option : [
+                              "A. Python",
+                              "B. Java",
+                              "C. HTML",
+                              "D. C++",
+                    ]
+
+          },
+          {
+                    numb :2,
+                    question :"In JavaScript, which keyword is used to declare a variable?",
+                    answer : "D. All of the above",
+                    Option : [
+                              "A. let",
+                              "B. var",
+                              "C. const",
+                              "D. All of the above",
+                    ]
+
+          },
+          {
+                    numb :3,
+                    question :"What is the correct way to create a function in Python?",
+                    answer : "B. def myFunction():",
+                    Option : [
+                              "A. function myFunction():",
+                              "B. def myFunction():",
+                              "C. function: myFunction()",
+                              "D. create myFunction()",
+                    ]
+
+          },
+          {
+                    numb :4,
+                    question :"Which of these is not a database management system?",
+                    answer : "D. TensorFlow",
+                    Option : [
+                              "A. MySQL",
+                              "B. Oracle",
+                              "C. MongoDB",
+                              "D. TensorFlow",
+                    ]
+
+          },
+          {
+                    numb :5,
+                    question :"What does SQL stand for?",
+                    answer : "A. Structured Query Language",
+                    Option : [
+                              "A. Structured Query Language",
+                              "B. Simple Query Language",
+                              "C. Strong Query Language",
+                              "D. Sequential Query Language",
+                    ]
+
+          },
+          {
+                    numb :6,
+                    question :"Which of the following is not a data type in JavaScript?",
+                    answer : "B. Integer",
+                    Option : [
+                              "A. Boolean",
+                              "B. Integer",
+                              "C. Undefined",
+                              "D. String",
+                    ]
+
+          },
+          {
+                    numb :7,
+                    question :"What is the main purpose of the return statement in a function?",
+                    answer : "C. To send a value back to the function caller",
+                    Option : [
+                              "A. To stop the execution of the program",
+                              "B. To output values to the console",
+                              "C. To send a value back to the function caller",
+                              "D. To define a variable",
+                    ]
+
+          },
+          {
+                    numb :8,
+                    question :"Which of these is used to declare constants in JavaScript?",
+                    answer : "A. const",
+                    Option : [
+                              "A. const",
+                              "B. var",
+                              "C. let",
+                              "D. define",
+                    ]
+
+          },
+          {
+                    numb :9,
+                    question :"What is the default value of a boolean variable in Java?",
+                    answer : "C. false",
+                    Option : [
+                              "A. 0",
+                              "B. null",
+                              "C. false",
+                              "D. true",
+                    ]
+
+          },
+          {
+                    numb :10,
+                    question :"Which operator is used to concatenate strings in Python?",
+                    answer : "A. +",
+                    Option : [
+                              "A. +",
+                              "B. -",
+                              "C. &",
+                              "C. %",
+                    ]
+
+          },
+ 
+];
